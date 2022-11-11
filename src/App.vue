d63b65fda2f9370be56d4ae31b0fb5f7b6acec3b
黄永粤 <newflyhuang@gmail.com>
2022-11-11 15:48:51 +0800

    删除不必要的判断

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,11 @@
       </el-header>
       <el-main>
         <router-view v-slot="{ Component, route }">
-          <transition :name="route.meta.transition || 'fade'" mode="out-in">
+          <transition :name="'fade'" mode="out-in">
             <keep-alive>
               <component
                 :is="Component"
-                :key="route.meta.usePathKey ? route.path : undefined"
+                :key=" route.path"
               />
             </keep-alive>
           </transition>
