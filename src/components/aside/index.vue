ae4d6b3f79d0fe0a9ec8ac50c0b672e7a135ef53
黄永粤 <newflyhuang@gmail.com>
2022-11-14 15:05:38 +0800

    预留侧边栏展开控制

--- src/components/aside/index.vue
+++ src/components/aside/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <el-menu :default-active="defaultActive" class="menu-vertical" router>
-    <template v-for="(item, index) in menu">
-      <el-menu-item v-if="!item.meta.hidden" :index="item.path" :key="index">
-        {{ item.meta.name }}
-      </el-menu-item>
-    </template>
-  </el-menu>
+  <el-menu :default-active="defaultActive" class="menu-vertical" router :collapse="isCollapse">
+      <template v-for="(item, index) in menu">
+        <el-menu-item v-if="!item.meta.hidden" :index="item.path" :key="index">
+          <el-icon>
+            <component :is="item.meta.icon"></component>
+          </el-icon>
+          <span>
+            {{ item.meta.name }}
+          </span>
+        </el-menu-item>
+      </template>
+    </el-menu>  
+ 
 </template>
 
 <script setup>
@@ -13,6 +19,7 @@ import { ref, computed, onMounted, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 // 使用 ref 声明的 变量 需通过  变量名.value  = val  来赋值
 let defaultActive = ref("");
+const isCollapse = ref(false)
 const route = useRoute();
 const router = useRouter();
 const menu = computed(() => {
@@ -33,4 +40,8 @@ watch(
 .menu-vertical {
   height: 100%;
 }
+
+/* .menu-vertical:not(.el-menu--collapse) {
+  width: 200px;
+} */
 </style>
